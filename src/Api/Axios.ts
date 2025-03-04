5de37635decfe6db0be5db42d0c914f2f095310a
Jupiter Moon <albinbtg@gmail.com>
2025-03-04 08:21:00 +0530

    update

--- src/Api/Axios.ts
+++ src/Api/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:3000/api";
+export const BASE_URL = "https://swiftin-server.onrender.com/api";
 
 export const Axios = axios.create({
   baseURL: BASE_URL,
